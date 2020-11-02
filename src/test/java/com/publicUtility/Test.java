c74608ed6dba7c7756f35546295d93729f02f61e
VarvaraKrestyanova <krestyanovav@gmail.com>
2020-11-02 10:40:12 +0300

    test

--- src/test/java/com/publicUtility/Test.java
+++ src/test/java/com/publicUtility/Test.java
@@ -7,14 +7,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Test {
-  /* @org.testng.annotations.Test
-    public void userCanLoginByUsername() {
-        open("https://e-dostavka.by/");
-        $(By.name("user.name")).setValue("johny");
-        $("#submit").click();
-        $(".loading_progress").should(disappear); // Waits until element disappears
-        $("#username").shouldHave(text("Hello, Johny!")); // Waits until element gets text
-    }*/
 
     @org.testng.annotations.Test
     public void eDostavka() {
