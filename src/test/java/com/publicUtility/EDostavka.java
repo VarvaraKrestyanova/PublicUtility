882b98325def4485f0f75d376791bffbec618f9b
VarvaraKrestyanova <krestyanovav@gmail.com>
2020-11-03 10:48:04 +0300

    5 items are added

--- src/test/java/com/publicUtility/EDostavka.java
+++ src/test/java/com/publicUtility/EDostavka.java
@@ -2,6 +2,7 @@ package com.publicUtility;
 
 import com.codeborne.selenide.Condition;
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -25,6 +26,12 @@ public class EDostavka {
         String actualResult = $(By.xpath("//span[.='54.2']")).getText();
         System.out.println("Test1. Ground coffee");
         System.out.println("Expected Result: " + "54.2" + "\n" + "Actual Result: " + actualResult);
+    }
+
+
+    @BeforeMethod
+    public void greeting(){
+        System.out.println("I'm king here. And then there will be new tests.");
     }
 
     @Test
@@ -62,7 +69,7 @@ public class EDostavka {
         $(By.xpath("//img[@alt='Кумкват «Равит» 250 г.']")).click();
         $(By.cssSelector("div[itemprop='offers']")).shouldHave(Condition.text("10р.99к."));
         String actualResult = $(By.cssSelector("div[itemprop='offers']")).getText();
-        System.out.println("Test3. Kumquat");
+        System.out.println("Test4. Kumquat");
         System.out.println("Expected Result: " + "10р.99к." + "\n" + "Actual Result: " + actualResult);
     }
 
@@ -75,7 +82,7 @@ public class EDostavka {
         $(By.xpath("//img[@alt='Тетрадь школьная «Rikki» широкая клетка, 12 листов.']")).click();
         $(By.cssSelector("div[itemprop='offers']")).shouldHave(Condition.text("0р.14к."));
         String actualResult = $(By.cssSelector("div[itemprop='offers']")).getText();
-        System.out.println("Test3. Notebook");
+        System.out.println("Test5. Notebook");
         System.out.println("Expected Result: " + "0р.14к." + "\n" + "Actual Result: " + actualResult);
     }
 
@@ -88,7 +95,7 @@ public class EDostavka {
         $(By.xpath("//div[@class='img']//img[1]")).click();
         $(By.cssSelector("div[itemprop='offers']")).shouldHave(Condition.text("14р.79к."));
         String actualResult = $(By.cssSelector("div[itemprop='offers']")).getText();
-        System.out.println("Test3. Artichoke");
+        System.out.println("Test6. Artichoke");
         System.out.println("Expected Result: " + "14р.79к." + "\n" + "Actual Result: " + actualResult);
     }
 
